93a7d2b47336a7871a2b51a5e5ee8c65af183b6e
jdvolk <justin.d.volk@gmail.com>
2020-06-02 19:42:05 -0600

    remove comments

--- src/pantry.js
+++ src/pantry.js
@@ -102,21 +102,9 @@ class Pantry {
       let err = JSON.parse(error);
       console.error(err)
     })
-    /*
-    POST /api/v1/whats-cookin/1911/users/wcUsersData/?userID=1 HTTP/1.1
-    Host: fe-apps.herokuapp.com
-    Content-Type: application/json
-    Content-Length: 75
-
-{
-  "userID" : 1,
-  "ingredientID" : 18371,
-  "ingredientModification": 7
-}
-    */
   }
+
   addIngredientsToPantry(recipe) {
-    // Add the necessary ingredients to my pantry
     let ingredientsNeeded = this.returnIngredientsNeeded(recipe)
     ingredientsNeeded.forEach(ingredient => {
       let inPantry = this.returnIngredient(ingredient.id, this.pantry);
@@ -130,28 +118,11 @@ class Pantry {
         this.postToPantry(ingredient.id, ingredient.amount)
       }
     })
-    // call the method that get the ingredients still needed
-    // add those amounts to the pantry
-    // if the ingredient is in the pantry, then just add the amounts to whats there
-    // if not then add that ingredient to the pantry
-        // post the new pantry to the api using fetch
-
-    // (and keep this up to date with the database via fetch)
   }
+
   removeIngredientsUsed() {}
 }
 
 
 export default Pantry;
 
-/*
-Every User should have a pantry. A Pantry holds on to all the ingredients 
-its owner has stocked, and the amount of each ingredient they have. As a user, I should be able to:
-
-Determine whether my pantry has enough ingredients to cook a given meal
-
-Determine the amount of ingredients still needed to cook a given meal, based on what’s in my pantry
-Determine how much it will cost to buy the necessary ingredients needed to cook a given meal, based on what’s in my pantry
-Add the necessary ingredients to my pantry (and keep this up to date with the database via fetch)
-Remove the ingredients used for a given meal from my pantry, once that meal has been cooked
-*/
