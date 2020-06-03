7db20460d8604b08ad4d67549a8c1f6275b5de91
jdvolk <justin.d.volk@gmail.com>
2020-06-02 20:22:22 -0600

    add spies test for dom uppdates

--- test/domUpdates-test.js
+++ test/domUpdates-test.js
@@ -7,7 +7,6 @@ let domUpdates
 
 describe('DomUpdates', () => {
   beforeEach(() => {
-    domUpdates = new DomUpdates()
     global.document = {}
     chai.spy.on(document, ['insertAdjacentHTML'], () => {})
   })
@@ -15,5 +14,47 @@ describe('DomUpdates', () => {
   it.only('Should be a function', () => {
     expect(DomUpdates).to.be.a('function');
   });
+
+  it('should invoke on startup', () => {
+    DomUpdates.onStartUp()
+    expect(DomUpdates.onStartUp).to.have.been.called(1);
+  })
+  it('should invoke populate cards', () => {
+    DomUpdates.populateCards()
+    expect(DomUpdates.populateCards).to.have.been.called(1);
+  })
+  it('should invoke view favorites', () => {
+    DomUpdates.viewFavorites()
+    expect(DomUpdates.viewFavorites).to.have.been.called(1);
+  })
+  it('should invoke viewSaved', () => {
+    DomUpdates.viewSaved()
+    expect(DomUpdates.viewSaved).to.have.been.called(1);
+  })
+  it('should invoke greetUser', () => {
+    DomUpdates.greetUser()
+    expect(DomUpdates.greetUser).to.have.been.called(1);
+  })
+  it('should invoke addCardToList', () => {
+    DomUpdates.addCardToList
+    expect(DomUpdates.addCardToList).to.have.been.called(1);
+  })
+  it('should invoke cardButtonConditionals', () => {
+    DomUpdates.cardButtonConditionals()
+    expect(DomUpdates.cardButtonConditionals).to.have.been.called(1);
+  })
+  it('should invoke display directions', () => {
+    DomUpdates.displayDirections()
+    expect(DomUpdates.displayDirections).to.have.been.called(1);
+  })
+  it('should invoke make recipes', () => {
+    DomUpdates.makeRecipes()
+    expect(DomUpdates.makeRecipes).to.have.been.called(1);
+  })
+  it('should invoke searchCards', () => {
+    DomUpdates.searchCards()
+    expect(DomUpdates.searchCards).to.have.been.called(1);
+  })
+
 })
 
